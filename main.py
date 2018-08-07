9480fbaece5d4cd4221952183c4c68120e609683
Dahyun <honeybeat1@gmail.com>
2018-08-07 16:08:53 +0900

    슬랙 연동

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
-print('chun jae ya?!')
-print('집에 갈까?')
+from rtmbot import Rtmbot
+from rtmbot.core import Plugin
+from secret import SLACK_TOKEN
+
+class HelloPlugin(Plugin):
+	def process_message(self,data):
+		if "애란" in data["text"]:
+			self.outputs.append([data["channel"],'불렀어?'])
+		print(data)
+
+
+config = {
+	"SLACK_TOKEN" : SLACK_TOKEN
+	"ACTIVE_PLUGINS" : ["main.HelloPlugin"]
+}
+bot = Rtmbot(config)
+bot.start()
+
